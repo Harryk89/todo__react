9e69d7bfcc9904e8519af35f583808552e1b06f5
Сапсай Артем Степанович <sapsaj_as@steko.net>
2018-11-09 10:58:09 +0200

    Updates

---
+++ precache-manifest.9e4a324a4bc606422c88f95aa69a2af4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "739eac4188ac927a30c3",
+    "url": "/todo__react/static/js/runtime~main.739eac41.js"
+  },
+  {
+    "revision": "ea3f583f7372c6388795",
+    "url": "/todo__react/static/js/main.ea3f583f.chunk.js"
+  },
+  {
+    "revision": "64e5021f6a750df8bae4",
+    "url": "/todo__react/static/js/1.64e5021f.chunk.js"
+  },
+  {
+    "revision": "ea3f583f7372c6388795",
+    "url": "/todo__react/static/css/main.41edf4e9.chunk.css"
+  },
+  {
+    "revision": "c98153054bde312bd6a442b7a7599c90",
+    "url": "/todo__react/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.e7693454168f2bf0046d271dee205410.js"
+  "/todo__react/precache-manifest.9e4a324a4bc606422c88f95aa69a2af4.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/todo__react/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
